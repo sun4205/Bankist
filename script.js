ee17a6d247f4e04674cf75eaced8c64f9fb93717
sun4205 <hanna4205@gmail.com>
2026-02-02 12:21:29 -0500

    fixing sorting bug

--- script.js
+++ script.js
@@ -84,14 +84,18 @@ const inputClosePin = document.querySelector('.form__input--pin');
 const displayMovements = function (acc, sort = false) {
   containerMovements.innerHTML = '';
 
-  const movs = sort
-    ? acc.movements.slice().sort((a, b) => a - b)
-    : acc.movements;
+  const combinedMovesDates = acc.movements.map((mov, i) => ({
+    movements: mov,
+    movementDate: acc.movementsDates.at(i),
+  }));
 
-  movs.forEach(function (mov, i) {
-    const type = mov > 0 ? 'deposit' : 'withdrawal';
+  if (sort) combinedMovesDates.sort((a, b) => a.movements - b.movements);
 
-    const date = new Date(acc.movementsDates[i]);
+  combinedMovesDates.forEach(function (obj, i) {
+    const { movements, movementDate } = obj;
+    const type = movements > 0 ? 'deposit' : 'withdrawal';
+
+    const date = new Date(movementDate);
     const day = `${date.getDate()}`.padStart(2, '0');
     const month = `${date.getMonth() + 1}`.padStart(2, '0');
     const year = date.getFullYear();
@@ -103,7 +107,7 @@ const displayMovements = function (acc, sort = false) {
       i + 1
     } ${type}</div>
     <div class="movements__date">${displayDate}</div>
-        <div class="movements__value">${mov.toFixed(2)}€</div>
+        <div class="movements__value">${movements.toFixed(2)}€</div>
       </div>
     `;
 
